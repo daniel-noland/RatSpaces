8766fa9460bb98833fa2c80dff5422eb03b50e24
Daniel Noland <daniel.noland@gmail.com>
2013-07-25 17:57:29 -0600

    Initial commit.
    See the README.md file for details about what this script does and why I
    wrote it.

---
+++ README.md
@@ -0,0 +1,76 @@
+Rat Spaces
+==========
+ 
+Author: Daniel Noland (daniel.noland+ratspaces@gmail.com)
+
+(I am most easily contacted via +Daniel Noland on Google +)
+
+This is a simple hack which helps me stomach the Ratpoison window
+manager's workspaces functionality, and generally aids in the
+manipulation of ratpoison windows.  I hope it is useful to someone
+else. 
+
+Usage
+-----
+
+The script is entirely self contained and requires only that you have
+ratpoison running (obviously).  The script can be called from the
+command line, but it is MUCH more effective to bind it to hot keys in
+your .ratpoisonrc file.
+
+The script accepts the following arguments:
+
+*scan*
+This displays a summary of all the windows in all the active workspaces.
+Like the built in 'windows' command, it places an \* next to the active
+window.  It also wraps the name of the active workspace with \*'s.
+
+*inc*
+Move to the next workspace. That is increment the workspace.
+
+*dec*
+Move to the previous workspace. That is decrement the workspace.
+
+*sendn*
+Send the current window to the next workspace.
+
+*sendp*
+Send the current window to the previous workspace.
+
+*follown*
+Send the current window to the previous workspace. Also, move to the
+next workspace.  That is, follow to the next workspace.
+
+*followp*
+Send the current window to the previous workspace. Also, move to the
+previous workspace.  That is, follow to the previous workspace.
+
+*swap*
+Swap the current window with the first selected window on the next
+monitor.  This is especially useful if you only have two monitors (which
+I suspect covers most of us).
+
+Example 
+-------
+
+You could (if you are like me) configure your .ratpoisonrc like so:
+
+...
+bind w ecec ratSpaces.sh scan
+bind comma exec ratSpaces.sh dec
+bind period exec ratSpaces.sh inc
+bind greater exec ratSpaces.sh sendn
+bind less exec ratSpaces.sh sendp
+bind C-period exec ratSpaces.sh follown
+bind C-comma exec ratSpaces.sh followp
+bind slash exec ratSpaces.sh swap
+...
+
+Then simply add the ratSpaces.sh script to your PATH and start
+experimenting.
+
+License
+-------
+
+I am putting this up under the MIT License.  See the LICENSE document
+for the full text of that license.
---
+++ ratSpaces.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+
+# Author: Daniel Noland
+# Date: 2013-07-25
+# This is a simple helper script which lets me find the ratpoison "implementation"
+# of work spaces to be tolerable.
+
+# Convenience functions
+# =====================
+
+# compute the total number of groups
+totalGroups() {
+   echo -e $(ratpoison -c "groups" | wc -l)
+}
+
+# return current group number
+getCurrentGroupNumber() {
+   echo -e $(ratpoison -c "groups" | sed -e "/\*[0-9]*/!d" -e "s/\*.*//")
+}
+
+# return current window number
+getCurrentWindowNumber() {
+   echo -e $(ratpoison -c "windows" | sed -e "/[0-9]*\*/!d" -e "s/\([0-9]*\)\*.*/\1/")
+}
+
+# return current group name
+getCurrentGroupName() {
+   echo -e $(ratpoison -c "groups" | sed -e "/[0-9]*\*/!d" -e "s/[0-9]*\*//")
+}
+
+# return current window name
+getCurrentWindowName() {
+   echo -e $(ratpoison -c "windows" | sed -e "/[0-9]*\*/!d" -e "s/[0-9]*\*\(.*\)/\1/")
+}
+
+# get next group number
+getNextGroupNumber() {
+   local group=$(( $(getCurrentGroupNumber) + 1 ))
+   local group=$(( $group % $(totalGroups) ))
+   echo $group
+}
+
+# get previous group number
+getPreviousGroupNumber() {
+   local group=$(( $(getCurrentGroupNumber) - 1 ))
+   [ $group -lt 0 ] && group=$(( $(totalGroups) - 1 ))
+   echo $group
+}
+
+# change to group number (provided by first argument),
+# update the screen to display a window specific to that group,
+# and print the current group scan
+changeToGroupNumber() {
+   #move to that group
+   ratpoison -c "gselect $1"
+   ratpoison -c "select -"
+   ratpoison -c "next"
+   scanAllGroups
+}
+
+# "Public Facing" Functions
+# =========================
+
+# return a formatted list of all the windows in all the groups
+# with marks to indicate the active window and group
+scanAllGroups() {
+   local initialGroup=$(getCurrentGroupNumber)
+   local output=""
+   for (( i=0; i<$(totalGroups); i++ ))
+   do
+      local mark="---"
+      [ $i -eq $initialGroup ] && mark="***"
+      ratpoison -c "gselect $i"
+      name=$(getCurrentGroupName)
+      output+=$mark$name$mark'\n'$(ratpoison -c "windows")'\n'
+   done
+   ratpoison -c "gselect $initialGroup"
+   ratpoison -c "echo $(echo -e ${output})"
+}
+
+# Send the current window to the next group
+sendWindowToNextGroup() {
+   ratpoison -c "gmove $(getNextGroupNumber)"
+   ratpoison -c "next"
+   scanAllGroups
+}
+
+# Send the current window to the previous group
+sendWindowToPreviousGroup() {
+   ratpoison -c "gmove $(getNextPreviousNumber)"
+   ratpoison -c "next"
+   scanAllGroups
+}
+
+# Send the current window to the next group and switch to the next group
+followWindowToNextGroup() {
+   ratpoison -c "gmove $(getNextGroupNumber)"
+   changeToGroupNumber $(getNextGroupNumber)
+   scanAllGroups
+}
+
+# Send the current window to the previous group and switch to the next group
+followWindowToPreviousGroup() {
+   ratpoison -c "gmove $(getPreviousGroupNumber)"
+   changeToGroupNumber $(getPreviousGroupNumber)
+   scanAllGroups
+}
+
+# swap the active window on this monitor with screen on the next
+swapScreens() {
+   local currentWindow=$(getCurrentWindowNumber)
+   ratpoison -c "select -"
+   ratpoison -c "nextscreen"
+   local otherWindow=$(getCurrentWindowNumber)
+   # this is not optional if you want it to correctly swap to blank
+   # windows.
+   ratpoison -c "select -"
+   ratpoison -c "select $currentWindow"
+   ratpoison -c "prevscreen"
+   ratpoison -c "select $otherWindow"
+}
+
+# pick action based on incoming argument
+case "$1" in
+
+   "scan")
+      scanAllGroups
+   ;;
+
+   "inc")
+      changeToGroupNumber $(getNextGroupNumber)
+   ;;
+
+   "dec")
+      changeToGroupNumber $(getPreviousGroupNumber)
+   ;;
+
+   "sendn")
+      sendWindowToNextGroup
+   ;;
+
+   "sendp")
+      sendWindowToPreviousGroup
+   ;;
+
+   "follown")
+      followWindowToNextGroup
+   ;;
+
+   "followp")
+      followWindowToPreviousGroup
+   ;;
+
+   "swap")
+      swapScreens
+   ;;
+
+   *)
+      echo "invalid option $1"
+      exit 1;
+   ;;
+
+esac
+
+exit 0
